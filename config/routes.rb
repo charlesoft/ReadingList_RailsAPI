aaf5789f10bcd329a115a0364d11e9b70826270e
charlesoft <charlees1509@hotmail.com>
2016-08-01 00:24:42 -0300

    Adding book listing

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ ReadingListRailsapi::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+    resources :books
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
